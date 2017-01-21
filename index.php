48d60b89da8fda5e042b4e8260a277427a15fa09
Linda Kovacs <lindaprovenza@yahoo.com>
2017-01-20 23:24:35 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,37 +4,27 @@
 
 <img src="images/weather.jpg">
 
-<div class="container contentContainer" id="topContainer">
-	<div class="row">
-		<div class="col-md-6 col-md-offset-3 center">
-			<h1 class="center white">Weather Scraper</h1>
+<div class="container">
+        <h1>What's The Weather?</h1>
+        <form>
+          <fieldset class="form-group">
+            <label for="city">Enter the name of a city.</label>
+            <input type="text" class="form-control" name="city" id="city" aria-describedby="emailHelp" placeholder="Eg. London, Tokyo" value="<?php
+                if(array_key_exists('city', $_GET)) {
+                    echo $_GET['city'];
+                }
+             ?>">
+          </fieldset>
 
-			<p class="lead center white">This application will predict the weather near you!</p>
-
-			<form>
-
-				<div class="form-group">
-					<input class="form-control" type="text" name="city" id="city" placeholder="Eg. London, Paris, San Francisco..." />
-				</div>
-
-				<button type="submit" class="btn btn-success btn-lg">Find My Weather</button>
-
-			</form>
-
-			<div id="success" class="alert alert-success">
-
-			</div>
-
-			<div id="danger" class="alert alert-danger">
-				Sorry, you entered a wrong city.
-			</div>
-
-			<div id="noCity" class="alert alert-danger">
-				Please enter a city.
-			</div>
-
-		</div>
-
-	</div>
-</div> <!-- End of .container -->
-
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+        <div id="weather">
+            <?php
+                if($weather) {
+                    echo '<div class="alert alert-success" role="alert">'.$weather.'</div>';
+                } else  if($error) {
+                    echo '<div class="alert alert-danger" role="alert">'.$error.'</div>';
+                }
+            ?>
+        </div>
+    </div>
