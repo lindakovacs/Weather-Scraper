3eb02508d0375533a8b7fc437895f28c233b0d01
Linda Kovacs <lindaprovenza@yahoo.com>
2017-01-20 22:35:48 -0500

    Create header.php

---
+++ includes/header.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+	<title>My First Webpage</title>
+
+	<meta charset="utf-8" />
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+   <link rel="stylesheet" href="css/style.css">
+  
+</head>
